016d8364ba622fdada8f61f855d5c6ce5d496d56
takuya.W <wakayama.azest.co.jp>
2024-02-08 15:26:07 +0900

    縦横のサイズ取得完了

--- resize_movie_sample.py
+++ resize_movie_sample.py
@@ -1,21 +1,55 @@
 import subprocess
 import sys
 
+# compression_file_path = "./output/output_compressed.mp4"
+def get_video_sizes(file_path):
+    input_ffmpeg_cmd = ['ffmpeg', '-i', file_path]  # 例としてFFmpegコマンドをリスト形式で渡す
+    result = subprocess.run(input_ffmpeg_cmd, capture_output=True, text=True)
+    output = result.stderr
+    lines = output.split('\n')
+    for line in lines:
+        if 'Stream #0:0' in line:
+            tokens = line.split(',')[2].strip().split()[0]
+            width, height = map(int, tokens.split('x'))
+            return width, height
+    return None, None
 
-# ファイルパスと圧縮後のファイルパス
-file_path = sys.argv[1]  # ファイルパスは第1引数として受け取る
-# file_path = "input.mp4"
-compression_file_path = "./output/output_compressed.mp4"
+file_path = sys.argv[1]
+print(file_path)
+width, height = get_video_sizes(file_path)
+print('縦:', height)
+print('横:', width)
 
-# 幅と高さ
-w = 1280
-h = 720
 
-# ffmpegコマンドの作成
-ffmpeg_cmd = f'ffmpeg -i {file_path} -vcodec libx264 -vb 2000k -s {w}x{h} -acodec aac -ab 64k -aac_coder twoloop -pix_fmt yuv420p -movflags +faststart {compression_file_path}'
 
-# ffmpegコマンドの実行
-subprocess.run(ffmpeg_cmd, shell=True)
+
+
+
+
+# input_ffmpeg_cmd = ['ffmpeg', '-i', file_path]
+# try:
+#     result = subprocess.run(input_ffmpeg_cmd, capture_output=True, text=True)
+#     output = result.stderr
+#     lines = output.split('\n')
+#     for line in lines:
+#         if 'Stream #0:0' in line:
+#             tokens = line.split(',')[2].strip().split()[0]
+#             width, height = map(int, tokens.split('x'))
+#             print('縦:',height)
+#             print('横:',width)
+#             break
+# except Exception as e:
+#     print("エラー:", e)
+
+# # 幅と高さ
+# w = 800
+# h = 720
+
+# # ffmpegコマンドの作成
+# ffmpeg_cmd = f'ffmpeg -i {file_path} -vcodec libx264 -vb 2000k -s {w}x{h} -acodec aac -ab 64k -aac_coder twoloop -pix_fmt yuv420p -movflags +faststart {compression_file_path}'
+
+# # ffmpegコマンドの実行
+# subprocess.run(ffmpeg_cmd, shell=True)
 
 
 # 実行時下記のコマンドを実行すると.pyファイルが実行されるはず
