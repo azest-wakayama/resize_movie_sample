c6b2612ac87033e3a672ac4d8fecc6c8dcc6acba
takuya.W <wakayama.azest.co.jp>
2024-02-09 13:40:19 +0900

    メソッドの順番調整

--- resize_movie_sample.py
+++ resize_movie_sample.py
@@ -2,7 +2,23 @@ import subprocess
 import sys
 import re
 
-compression_file_path = "./output/output_compressed.mp4"
+
+def resize_movie_sample(file_path):
+  movie_width, movie_height = get_video_sizes(file_path)
+  ratio_w, ratio_h = calculation_aspect_ratio(movie_width, height)
+  aspect_ratio = ratio_w / ratio_h
+#　横1920、縦1080以下の場合はそのままのサイズで返す
+  if movie_width <= 1920 and movie_height <= 1080:
+    return movie_width, movie_height
+# 縦長動画の場合
+  elif movie_width <= movie_height:     
+     resize_height = 1080
+     resize_width = resize_height * aspect_ratio
+     return resize_width ,resize_height
+  else:
+      resize_width = 1920
+      resize_height = resize_width * aspect_ratio
+      return resize_width ,resize_height
 
 def get_video_sizes(file_path):
     input_ffmpeg_cmd = ['ffmpeg', '-i', file_path]
@@ -32,22 +48,7 @@ def calculation_aspect_ratio(width, height):
     ratio_h = height / gcb_value
     return ratio_w, ratio_h
 
-def resize_movie_sample(file_path):
-  movie_width, movie_height = get_video_sizes(file_path)
-  ratio_w, ratio_h = calculation_aspect_ratio(movie_width, height)
-  aspect_ratio = ratio_w / ratio_h
-#　横1920、縦1080以下の場合はそのままのサイズで返す
-  if movie_width <= 1920 and movie_height <= 1080:
-    return movie_width, movie_height
-# 縦長動画の場合
-  elif movie_width <= movie_height:     
-     resize_height = 1080
-     resize_width = resize_height * aspect_ratio
-     return resize_width ,resize_height
-  else:
-      resize_width = 1920
-      resize_height = resize_width * aspect_ratio
-      return resize_width ,resize_height
+
      
 
 #   ratio_w, ratio_h = calculation_aspect_ratio(movie_width, movie_height)
@@ -70,6 +71,7 @@ print("Ratio Height:", ratio_h)
 # h = 720
 
 # # ffmpegコマンドの作成
+compression_file_path = "./output/output_compressed.mp4"
 ffmpeg_cmd = f'ffmpeg -i {file_path} -vcodec libx264 -vb 2000k -s {w}x{h} -acodec aac -ab 64k -aac_coder twoloop -pix_fmt yuv420p -movflags +faststart {compression_file_path}'
 
 # ffmpegコマンドの実行
