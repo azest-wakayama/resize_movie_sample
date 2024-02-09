92898e6db83a9e0325dde6a817ae2e70abae5437
takuya.W <wakayama.azest.co.jp>
2024-02-09 15:06:34 +0900

    高さを最大1080までにする対応

--- resize_movie_sample.py
+++ resize_movie_sample.py
@@ -6,9 +6,11 @@ import re
 def resize_movie(file_path):
   movie_width, movie_height = get_video_sizes(file_path)
   ratio_w, ratio_h = calculation_aspect_ratio(movie_width, movie_height)
-  aspect_ratio = ratio_w / ratio_h
+  aspect_ratio = ratio_w / ratio_h  # 初期のアスペクト比を計算
+
 #　横1920、縦1080以下の場合はそのままのサイズで返す
   if movie_width <= 1920 and movie_height <= 1080:
+    aspect_ratio = ratio_w / ratio_h
     return movie_width, movie_height
 # 縦長動画の場合
   elif movie_width <= movie_height:     
@@ -17,8 +19,15 @@ def resize_movie(file_path):
      return resize_width ,resize_height
   else:
       resize_width = 1920
-      resize_height = resize_width * aspect_ratio
-      return resize_width ,resize_height
+      resize_height = resize_width / aspect_ratio
+      if resize_height < 1080:
+        return resize_width, resize_height
+      else:
+        while resize_height >= 1080:
+            resize_width -= 1
+            print(resize_width)
+            resize_height = resize_width / aspect_ratio
+        return resize_width, resize_height
 
 # ビデオサイズを取得する
 def get_video_sizes(file_path):
@@ -60,16 +69,16 @@ resize_width, resize_height = resize_movie(file_path)
 print('幅',resize_width)
 print('高さ',resize_height)
 # # 幅と高さ
-# w = 800
-# h = 720
+# w = 1080
+# h = 608
 
 # # ffmpegコマンドの作成
-# compression_file_path = "./output/output_compressed.mp4"
+# compression_file_path = f"./output/{w}_{h}_.mp4"
 # ffmpeg_cmd = f'ffmpeg -i {file_path} -vcodec libx264 -vb 2000k -s {w}x{h} -acodec aac -ab 64k -aac_coder twoloop -pix_fmt yuv420p -movflags +faststart {compression_file_path}'
 
 # # ffmpegコマンドの実行
-# subprocess.run(ffmpeg_cmd, shell=True)
+subprocess.run(ffmpeg_cmd, shell=True)
 
 
 # 実行時下記のコマンドを実行すると.pyファイルが実行されるはず
-# python resize_movie_sample.py ./input/横長動画サンプル.mp4
+# python resize_movie_sample.py ./input/2400_1350_.mp4
